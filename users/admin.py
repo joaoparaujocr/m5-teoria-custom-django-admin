28cced7ece1c3fca02f21edcf234bf75cc0d971a
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2022-12-17 17:54:30 -0400

    code example

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = ("date_joined", "last_login")
+
+    fieldsets = (
+        ("Credenciais", {"fields": ("username", "password")}),
+        (
+            "Personal Info",
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "account_balance",
+                    "favorite_color",
+                    "is_married",
+                    "children",
+                )
+            },
+        ),
+        ("Permissions", {"fields": ("is_staff", "is_superuser")}),
+        ("Dates", {"fields": ("last_login", "date_joined")}),)
+
+admin.site.register(User, CustomUserAdmin)
